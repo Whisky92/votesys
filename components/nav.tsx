7d4aee8f496555bdeb1afd4d24ee96af46141f97
Oláh Tamás <olah.tomi.92@gmail.com>
2024-04-10 02:16:20 +0200

    Modified navigation bar, added functionality to buttons

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,39 +1,56 @@
-import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+'use client'
+// used source for popup: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup_form
+
+import React, { MouseEvent, useEffect } from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+import { votingTimeContext } from "@app/utils/is_voting_time_context";
 
 export default function Nav() {
+  let ids: Array<string>;
+  const router = useRouter();
+  const {canVote, setCanVote,
+         canSeeResults, setCanSeeResults} = useContext(votingTimeContext);
+
+  function openForm(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    if (canVote) {
+      router.push("/vote_id_field");
+    } else {
+      alert("The voting already ended or it has not started yet!");
+    }
+  }
+
+  function showResults(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    if (canSeeResults) {
+      router.push("/results");
+    } else {
+      alert("The voting has not ended yet!");
+    }
+  }
+
   return (
-    <Navbar className="w-full dark" maxWidth="full">
-      <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
+    <>
+      <Navbar className="w-full dark flex flex-row fixed top-0" maxWidth="full" isBlurred={false}>
+        <NavbarBrand>
+          <Link href="/">
+            <p className="text-white text-xl font-bold italic">VoteSys</p>
           </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+        </NavbarBrand>
+        <NavbarContent className="flex flex-row">
+          <NavbarItem className="bg-white basis-1/2" />
+          <NavbarItem className="basis-1/4">
+            <button onClick={openForm} 
+                    className="text-white text-xl font-bold">Vote</button>
+          </NavbarItem>
+          <NavbarItem className="basis-1/4">
+            <button onClick={showResults}
+                    className="text-white text-xl font-bold">Results</button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </>
   );
 }
